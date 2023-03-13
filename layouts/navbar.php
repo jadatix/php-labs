8b2033a9ad6b8abd8be4a6c96eb6ee8ecc54db69
Andrii <andrijlasenko@gmail.com>
2023-03-14 00:06:27 +0200

    start lab3

--- layouts/navbar.php
+++ layouts/navbar.php
@@ -1,7 +1,7 @@
 <nav>
     <ul>
         <li><a href="index.php?action=main">Home</a></li>
-        <li><a href="#">Shop</a></li>
+        <li><a href="index.php?action=registration">Sign up</a></li>
         <li><a href="index.php?action=about">About Us</a></li>
         <li><a href="#Contact">Contact Us</a></li>
     </ul>
