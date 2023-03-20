d80a31690a145da61b12ced6caa7a4be618a25ad
Andrii <andrijlasenko@gmail.com>
2023-03-21 00:12:21 +0200

    finished validation

--- views/registration.php
+++ views/registration.php
@@ -1,4 +1,4 @@
-<?php;
+<?php
     $messages = [
         "login" => "Login must contain at least 4 characters and can only contain letters and numbers.",
         "password" => "Password must contain at least 8 characters, at least one uppercase letter, one lowercase letter and one number.",
@@ -8,6 +8,7 @@
         "login" => "/^[a-zA-Z0-9]{4,}$/",
         "password" => "/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/",
         "email" => "/^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/"];
+    $skip_validation = ["gender","confirm_password"];
     $errors = [];
 
     function display_error($message) {
@@ -15,24 +16,28 @@
     }
     
     function validate_field($field, $regex, $message) {
-        global $valid;
-        global $validated;
         global $errors;
-        // print_r(!preg_match($regex, $_POST[$field]));
-        // echo '<br>';
-        if(isset($_POST) && !preg_match($regex, $_POST[$field])) {
+        if(!preg_match($regex, $_POST[$field])) {
             $errors[$field] = $message;
         }
     }
-
-    foreach($_POST as $key => $value) {
-        // print_r("key:" . $key);
-        // print_r("field:" . $value);
-        // print_r("reg:" . $$regexes[$value]);
-        validate_field($key, $regexes[$value], $messages[$value]);
-    }
-    print_r($errors);
     
+    if(isset($_POST) && !empty($_POST)) {
+        if($_POST["password"] != $_POST["confirm_password"]) {
+            $errors["confirm_password"] = $messages["confirm_password"];
+        }
+        foreach($_POST as $key => $value) {
+            if(!in_array($key, $skip_validation) ){
+                print_r("key: ".$key);
+                print_r("<br>");
+                validate_field($key, $regexes[$key], $messages[$key]);
+            }
+        }
+    }
+    
+
+    print_r($errors);
+    print_r("<br>");
     if(empty($errors) && !empty($_POST)) {
         header('Location: index.php?action=main');
         exit();
@@ -44,22 +49,22 @@
     <div class="field-container">
         <label for="login">Login</label>
         <input type="text" name="login" />
-        <?php if(in_array('login', $errors)) display_error($errors['login']) ?>
+        <?php display_error($errors["login"]) ?>
     </div>
     <div class="field-container">
         <label for="password">Password</label>
         <input type="password" name="password" />
-        <?php if(in_array('password', $errors)) display_error($errors['password']) ?>
+        <?php display_error($errors["password"]) ?>
     </div>
     <div class="field-container">
         <label for="confirm_password">Confirm password</label>
         <input type="password" name="confirm_password" />
-        <?php if(in_array('confirm_password', $errors)) display_error($errors['confirm_password']) ?>
+        <?php display_error($errors["confirm_password"]) ?>
     </div>
     <div class="field-container">
         <label for="email">Email</label>
         <input type="text" name="email" />
-        <?php if(in_array('email', $errors)) display_error($errors['email']) ?>
+        <?php display_error($errors["email"]) ?>
     </div>
     <fieldset>
         <legend>Gender</legend>
