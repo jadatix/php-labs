c127be58e9278fa47a3ea1875885c6c9a7805bbf
Liashenko Mykhailo <nightmisha8@gmail.com>
2023-10-14 21:26:32 +0300

    added not working docker files

--- index.php
+++ index.php
@@ -4,7 +4,11 @@
     // ini_set('display_errors', 1);
     require_once("layouts/header.php");
     require_once("functions/user_sql_functions.php");
-    $connection = mysqli_connect("localhost","root","","plant_shop");
+    $connection = mysqli_connect(
+        getenv('DB_HOST'), 
+        getenv('DB_USER'), 
+        getenv('DB_PASSWORD'), 
+        getenv('DB_NAME'));
 
     if(isset($_GET["action"]) && file_exists("views/". $_GET["action"] . ".php") ) {
         include_once("views/". $_GET["action"] . ".php");
